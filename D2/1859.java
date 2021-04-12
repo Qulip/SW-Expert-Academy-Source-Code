6dd38a04400aae515dbf1b3a0811c346d7c7fb5f
Qulip <ilkwon.ryu@gmail.com>
2021-04-12 23:28:38 +0900

    7/10 Right 1859

--- D2/1859.java
+++ D2/1859.java
@@ -9,16 +9,20 @@ public class Solution {
         for(int tc = 1; tc<=T; tc++){
             int n = Integer.parseInt(br.readLine());
             int[] prices = new int[n];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=0; i<n; i++){
-                prices[i] = Integer.parseInt(st.nextToken());
+            int temp = 0;
+            while(temp<n){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                while(st.hasMoreTokens()){
+                    prices[temp] = Integer.parseInt(st.nextToken());
+                    temp++;
+                }
             }
             int nums = 0;
             int money = 0;
             int profit = 0;
             int i = n-1;
             while(i>0){
-                System.out.println(i);
+                //System.out.println("1st "+i);
                 int iprice = prices[i];
                 int iday = i;
                 if(iprice<=prices[i-1]){
@@ -26,6 +30,7 @@ public class Solution {
                     continue;
                 }
                 while(iprice>prices[i-1]){
+                    //System.out.println("2nd "+i);
                     i--;
                     if(i<=0){
                         break;
@@ -33,15 +38,16 @@ public class Solution {
                 }
                 if(i>=0){
                     for(int j=i; j<iday; j++){
-                        money=money+prices[i];
+                        money=money+prices[j];
                         nums++;
+                        //System.out.println(money+" "+prices[j]);
                     }
-                    profit = profit+iprice*nums - money;
+                    profit = profit + (iprice*nums) - money;
                 }
                 nums = 0;
                 money = 0;
             }
-            sb.append("#"+tc+" "+profit+"\ns");
+            sb.append("#"+tc+" "+profit+"\n");
         }
         System.out.println(sb);
     }
