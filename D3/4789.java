47aee44b2247256854ecaae768a3e858572d339b
RYU IL KWON <ryu@RYUs-iMac-Pro.local>
2021-04-13 23:27:17 +0900

    Make .gitignore

---
+++ D3/4789.java
@@ -0,0 +1,30 @@
+package swea4789;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        char[] people;
+        for(int tc = 1; tc <= T; tc++){
+            people = br.readLine().toCharArray();
+            int num = 0;
+            int rst = 0;
+            for(int i=0; i<people.length; i++){
+                if(people[i]-'0'>0){
+                    num = num+people[i]-'0';
+                }else{
+                    if(num<i+1){
+                        rst++;
+                    }
+                }
+                System.out.println(num);
+            }
+            sb.append("#"+tc+" "+rst+"\n");
+            System.out.println();
+        }
+        System.out.print(sb);
+    }
+}
