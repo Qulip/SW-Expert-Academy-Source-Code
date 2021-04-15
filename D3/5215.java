933a157fc0caea7bbe27a40f4494cc7167dd9b9d
Qulip <77991314+Qulip@users.noreply.github.com>
2021-04-15 23:34:00 +0900

    Solving 5215

---
+++ D3/5215.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int n,l,rst;
+    static int[] kcal, grade;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc<=T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            l = Integer.parseInt(st.nextToken());
+            kcal = new int[n];
+            grade = new int[n];
+            for(int i=0; i<n; i++){
+                st = new StringTokenizer(br.readLine());
+                grade[i] = Integer.parseInt(st.nextToken());
+                kcal[i] = Integer.parseInt(st.nextToken());
+            }
+            dfs(0,0, 0);
+            sb.append("#"+tc+" "+rst+"\n");
+        }
+        System.out.println(sb);
+    }
+    static void dfs(int now_kcal,int now_grade, int depth){
+        for(int i = depth; i<kcal.length; i++){
+            int temp = now_kcal + kcal[depth];
+            if(temp >= l){
+                if(temp == l){
+                    if(rst<now_grade+grade[i]){
+                        System.out.println(now_kcal+" "+now_grade+" "+rst+" "+(now_grade+grade[i]));
+                        rst = now_grade+grade[i];
+                    }
+                }
+            }else {
+                dfs(temp,now_grade+grade[i], depth + 1);
+            }
+            dfs(now_kcal,now_grade,depth+1);
+        }
+    }
+}
+/*
+1
+5 1000
+100 200
+300 500
+250 300
+500 1000
+400 400
+
+ */
