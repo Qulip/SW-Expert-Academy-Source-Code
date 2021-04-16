cfa571c7a7c8832bc743a1ac12a6d1d8a006c2b9
Qulip <ilkwon.ryu@gmail.com>
2021-04-17 00:43:37 +0900

    Solved 3282

--- D3/3282.java
+++ D3/3282.java
@@ -4,39 +4,30 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int[] v, c;
-    static int n, k, rst;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for(int tc = 1; tc <= T; tc++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            v = new int[n];
-            c = new int[n];
-            rst=0;
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] v = new int[n];
+            int[] c = new int[n];
+            int[] best = new int[k+1];
             for(int i = 0; i<n; i++){
                 st = new StringTokenizer(br.readLine());
                 v[i] = Integer.parseInt(st.nextToken());
                 c[i] = Integer.parseInt(st.nextToken());
             }
-            chkbest(0,0, 0);
-            sb.append("#"+tc+" "+rst+"\n");
+            for(int i=0; i<n; i++){
+                for(int j=k; j>=v[i]; j--){
+                    best[j] = Math.max(best[j], c[i]+best[j-v[i]]);
+                }
+            }
+            sb.append("#"+tc+" "+best[k]+"\n");
         }
         System.out.print(sb);
-    }
-    static void chkbest(int depth,int now_v, int now_c){
-        if(now_v>k){
-            return;
-        }
-        if(depth == n){
-            rst = Math.max(rst, now_c);
-            return;
-        }
-        chkbest(depth+1,now_v+v[depth],now_c+c[depth]);
-        chkbest(depth,now_v,now_c);
     }
 }
 /*
